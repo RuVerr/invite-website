251e76d2d82fed2cc9a2c291d14245be801a6016
RuVerr <ruvermishyan@gmail.com>
2026-09-06 21:19:46 +0400

    first commit

---
+++ app/(clients)/armen-lili/page.tsx
@@ -0,0 +1,10 @@
+import MinimalTemplate from "@/template/minimal/MinimalTemplate";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <MinimalTemplate />
+    </>
+  );
+}
